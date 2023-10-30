dcb72f56575b8d019a558da79ce01d67f2f1f968
LorenzoDalmau <lorenzodalmau1@gmail.com>
2023-10-30 22:28:32 +0100

    Register Screen Implementado

--- lib/ui/screens/RegisterScreen/register_screen.dart
+++ lib/ui/screens/RegisterScreen/register_screen.dart
@@ -11,6 +11,7 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  late Size mediaSize;
   // Controllers for the text fields
   TextEditingController usernameController = TextEditingController();
   TextEditingController emailController = TextEditingController();
@@ -55,133 +56,165 @@ class _RegisterScreenState extends State<RegisterScreen> {
   }
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    mediaSize = MediaQuery.of(context).size;
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            colors: [
-              Color(0xFF195F47),
-              Color.fromARGB(255, 20, 134, 94),
-            ],
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          // image: AssetImage("assets/images/background.png"),
+          image: AssetImage("assets/travel3.jpg"),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+            Color.fromARGB(192, 25, 95, 71),
+            BlendMode.darken,
           ),
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            const SizedBox(height: 10),
-            const Padding(
-              padding: EdgeInsets.all(20),
-              child: Icon(
-                Icons.logo_dev_rounded,
-                size: 150,
-                color: Colors.white,
-              ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            Positioned(
+              top: 40,
+              child: _buildTop(),
             ),
-            Expanded(
-              child: SingleChildScrollView(
-                reverse: true,
-                keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior
-                    .onDrag, // Controla el comportamiento del teclado
-                child: Container(
-                  decoration: const BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: Color.fromARGB(150, 0, 0, 0),
-                        blurRadius: 50,
-                      ),
-                    ],
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(50),
-                      topRight: Radius.circular(50),
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 30, right: 30, top: 10, bottom: 80),
-                    child: Column(
-                      children: <Widget>[
-                        const SizedBox(height: 40),
-                        // Username field
-                        TextFormField(
-                          controller: usernameController,
-                          decoration: const InputDecoration(
-                            hintText: 'Username',
-                          ),
-                        ),
-                        const SizedBox(height: 40),
-                        // Email field
-                        TextFormField(
-                          controller: emailController,
-                          decoration: const InputDecoration(
-                            hintText: 'email@example.com',
-                          ),
-                        ),
-
-                        const SizedBox(height: 40),
-                        // Password field
-                        TextFormField(
-                          controller: passwordController,
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                            hintText: 'Password',
-                          ),
-                        ),
-
-                        const SizedBox(height: 40),
-                        // Password confirmation
-                        TextFormField(
-                          controller: passwordConfirmationController,
-                          obscureText: true,
-                          decoration: const InputDecoration(
-                            hintText: 'Password confirmation',
-                          ),
-                        ),
-
-                        const SizedBox(height: 80),
-                        // Register button
-                        ElevatedButton(
-                          onPressed: () {
-                            if (passwordController.text.toString() ==
-                                passwordConfirmationController.text
-                                    .toString()) {
-                              registerUser(
-                                  usernameController.text.toString(),
-                                  emailController.text.toString(),
-                                  passwordController.text.toString());
-                            } else {
-                              Get.snackbar(
-                                  'Error', 'Las contraseñas no coinciden',
-                                  snackPosition: SnackPosition.BOTTOM);
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xFF195F47),
-                            padding:
-                                const EdgeInsets.symmetric(horizontal: 80),
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                            elevation: 20,
-                          ),
-                          child: const Text(
-                            'Register',
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
+            Positioned(
+              bottom: -3,
+              child: _buildBotton(),
             ),
           ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTop() {
+    return SizedBox(
+      width: mediaSize.width,
+      child: const Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image(
+            image: AssetImage("assets/Nomad.png"),
+            width: 200,
+            height: 200,
+          ), 
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBotton() {
+    return SizedBox(
+      width: mediaSize.width,
+      child: Card(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(50),
+            topRight: Radius.circular(50),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: _buildForm(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildForm() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Welcome',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        _buildGreenText("Please register with your information"),
+        const SizedBox(height: 25),
+        _buildGreenText("Username"),
+        _buildInputField(usernameController),
+        const SizedBox(height: 20),
+        _buildGreenText("Email Address"),
+        _buildInputField(emailController),
+        const SizedBox(height: 20),
+        _buildGreenText("Password"),
+        _buildInputField(passwordController, isPassword: true),
+        const SizedBox(height: 20),
+        _buildGreenText("Password Confirmation"),
+        _buildInputField(passwordConfirmationController, isPassword: true),
+        const SizedBox(height: 20),
+        _buildRegisterButton(),
+      ],
+    );
+  }
+
+  Widget _buildGreenText(String text) {
+    return Text(
+      text,
+      style: const TextStyle(
+        color: Color(0xFF195F47),
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _buildInputField(TextEditingController controller,
+      {isPassword = false}) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        suffixIcon: isPassword
+            ? const Icon(Icons.remove_red_eye)
+            : const Icon(Icons.person_2_outlined),
+      ),
+      obscureText: isPassword,
+    );
+  }
+
+  // Register Button
+  Widget _buildRegisterButton() {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          if (passwordController.text.toString() ==
+              passwordConfirmationController.text.toString()) {
+            registerUser(
+                usernameController.text.toString(),
+                emailController.text.toString(),
+                passwordController.text.toString());
+          } else {
+            Get.snackbar('Error', 'Las contraseñas no coinciden',
+                snackPosition: SnackPosition.BOTTOM);
+          }
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xFF195F47),
+          padding: const EdgeInsets.symmetric(horizontal: 80),
+          textStyle: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          elevation: 20,
+        ),
+        child: const Text(
+          'Register',
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
       ),
     );
