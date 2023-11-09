0ed9c7fa8b15f9d65135e690553a901c2439890c
oscar.perona.7e6 <oscar.perona.7e6@itb.cat>
2023-11-09 15:57:05 +0100

    ResetLocations

--- lib/models/Location.dart
+++ lib/models/Location.dart
@@ -1,14 +1,10 @@
 class LocationData{
 
   int id;
-  String name;
+  int creator_id;
   String description;
-  DateTime date;
-  double longitude;
-  double latitude;
-  int countryId;
-  int imageId;
+  int likes;
+  List<String> images;
 
-
-  LocationData(this.id, this.name, this.description, this.date, this.longitude, this.latitude, this.countryId, this.imageId);
+  LocationData(this.id, this.creator_id, this.description, this.likes, this.images);
 }
